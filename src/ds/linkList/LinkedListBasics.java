50e8bfc145cc5817d6ee4f8631912cd64e689809
Omnath Shinde <omnathshinde1@gmail.com>
2026-02-01 14:21:40 +0530

    Add LinkedListBasics

---
+++ src/ds/linkList/LinkedListBasics.java
@@ -0,0 +1,52 @@
+package ds.linkList;
+
+public class LinkedListBasics {
+    static class ListNode {
+        int data;
+        ListNode next;
+
+        ListNode(int value) {
+            this.data = value;
+            this.next = null;
+        }
+    }
+
+    static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.data + " -> ");
+            head = head.next;
+        }
+        System.out.println("null");
+    }
+
+    static ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    public static void main(String[] args) {
+// Creating Linked List: 1 -> 2 -> 3 -> 4 -> 5
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        System.out.println("Original Linked List:");
+        printList(head);
+
+
+        System.out.println("Reversed Linked List:");
+        head = reverseList(head);
+        printList(head);
+
+    }
+}
